3a138350fe63bd005b21408fc563d7abf9229b5f
Aman Joshi <aman.joshi@crownstack.com>
2022-05-11 06:43:12 +0530

    Initial Commit

---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by Aman Joshi on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
